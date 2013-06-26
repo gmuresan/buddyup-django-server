3b98422339889cd32735b93688dac56aa67733aa
George Muresan <epreneur@web405.webfaction.com>
2013-06-26 01:33:04 +0000

    Server settings

--- buddyup/settings.py
+++ buddyup/settings.py
@@ -82,11 +82,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/epreneur/webapps/buddyup_static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://epreneur.webfactional.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
