1e708639e88298315362e9886fedf74d9bfd554b
George Muresan <gmuresan@bitbucket.org>
2013-07-03 14:46:59 -0400

    Did unit tests and api server functions through getMessages

--- buddyup/settings.py
+++ buddyup/settings.py
@@ -1,5 +1,4 @@
 
-USE_TZ = True
 
 FACEBOOK_APP_ID = '142269385966400'
 FACEBOOK_APP_SECRET = 'fe9ae4f096756dfd59e5ed2d5225e552'
@@ -51,7 +50,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
--- status/models.py
+++ status/models.py
@@ -3,14 +3,16 @@ from userprofile.models import UserProfile, Group
 from django.contrib.gis.db import models as geomodels
 
 
-class Status(models.Model):
-    user = models.ForeignKey(UserProfile, related_name='statuses')
-    date = models.DateTimeField(auto_now=True, db_index=True)
-    expires = models.DateTimeField(db_index=True)
-    text = models.CharField(max_length=100, db_index=True)
+class Status(geomodels.Model):
+    user = geomodels.ForeignKey(UserProfile, related_name='statuses')
+    date = geomodels.DateTimeField(auto_now=True, db_index=True)
+    expires = geomodels.DateTimeField(db_index=True)
+    text = geomodels.CharField(max_length=100, db_index=True)
 
-    location = models.ForeignKey('Location', related_name='statuses', null=True, blank=True)
-    groups = models.ManyToManyField(Group, related_name='receivedStatuses', null=True, blank=True)
+    location = geomodels.ForeignKey('Location', related_name='statuses', null=True, blank=True)
+    groups = geomodels.ManyToManyField(Group, related_name='receivedStatuses', null=True, blank=True)
+
+    objects = geomodels.GeoManager()
 
 
 class Location(geomodels.Model):
@@ -21,7 +23,7 @@ class Location(geomodels.Model):
 
     address = geomodels.CharField(max_length=40, db_index=True, null=True, blank=True)
     city = geomodels.CharField(max_length=30, db_index=True, null=True, blank=True)
-    state = models.CharField(max_length=2, db_index=True, null=True, blank=True)
+    state = geomodels.CharField(max_length=2, db_index=True, null=True, blank=True)
 
     class Meta:
         index_together = [
