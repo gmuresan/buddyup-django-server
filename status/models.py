a7fa66cbbf5150a2825510ddbd9febb77328bdf3
George Muresan <gmuresan@bitbucket.org>
2013-07-30 16:11:05 -0400

    Set ording for statuses by date created - descending. (Most recent dates first)

--- status/models.py
+++ status/models.py
@@ -4,6 +4,9 @@ from django.contrib.gis.db import models as geomodels
 
 
 class Status(geomodels.Model):
+    class Meta:
+        ordering = ['-date']
+
     def __unicode__(self):
         username = self.user.user.username
         return "{0} - {1}".format(username, self.text)
