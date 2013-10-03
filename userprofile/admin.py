fc3c4bf3e81cea715ee68dcd97ef368f2a1e5850
George Muresan <gmuresan@bitbucket.org>
2013-10-02 20:30:20 -0400

    Added feedback to admin site

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from userprofile.models import UserProfile, Group
+from userprofile.models import UserProfile, Group, Feedback
 
 
 admin.site.register(UserProfile)
 admin.site.register(Group)
+admin.site.register(Feedback)
+
+
