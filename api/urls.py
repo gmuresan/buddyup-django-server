088e740a9999fcc708fd80743ff77dd443a76b90
George Muresan <gmuresan@bitbucket.org>
2014-01-13 23:28:50 -0500

    Django couldnt resolve urls after refactoring.

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,5 @@
 from django.conf.urls import patterns, url, include
-from api.facebookGraphObjects import fbObjectStatus
-from userprofile.api_functions import *
-from chat.api_functions import *
-from status.api_functions import *
 
 urlpatterns = patterns('',
-                       url(r'^/', include('userprofile.urls')),
-                       url(r'^/', include('chat.urls')),
-                       url(r'^/', include('status.urls')),
-
 
 )
--- buddyup/urls.py
+++ buddyup/urls.py
@@ -7,7 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'buddyup.views.home', name='home'),
-    url(r'^api/', include('api.urls')),
+    url(r'^api/', include('chat.urls')),
+    url(r'^api/', include('userprofile.urls')),
+    url(r'^api/', include('status.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
