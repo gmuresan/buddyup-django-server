8d57fcec8ac84ccfa2f5c5e1f043163b46fd0a7c
George Muresan <gmuresan@bitbucket.org>
2014-01-22 14:59:54 -0500

    Trying to fix friends of friends query

--- status/helpers.py
+++ status/helpers.py
@@ -43,7 +43,8 @@ def getNewStatusesJsonResponse(userProfile, since, lat=None, lng=None, radius=No
 
     inVisibleList = Status.objects.filter(Q(friendsVisible=userProfile))
     friendsStatuses = Status.objects.filter(Q(user__in=friends, visibility=Status.VIS_FRIENDS))
-    friendsOfFriendsStatuses = Status.objects.filter(user__in=friendsOfFriends, visibility=Status.VIS_FRIENDS_OF_FRIENDS)
+    friendsOfFriendsStatuses = Status.objects.filter(
+        Q(user__in=friendsOfFriends, visibility=Status.VIS_FRIENDS_OF_FRIENDS))
 
     publicStatuses = None
     if lat is not None and lng is not None:
