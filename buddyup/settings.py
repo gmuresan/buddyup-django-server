7712dc9b8bd56a5779bd87afb0634a15f13efd83
George Muresan <gmuresan@bitbucket.org>
2014-03-06 13:48:27 -0500

    Changed database

--- buddyup/settings.py
+++ buddyup/settings.py
@@ -37,7 +37,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'buddyup_development', # Or path to database file if using sqlite3.
+        'NAME': 'buddyup_beta', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'dev',
         'PASSWORD': 'buddyupdev',
