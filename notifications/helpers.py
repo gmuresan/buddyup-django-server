018fa2d1813c3f1304a14872a5374c4ab444c783
George Muresan <gmuresan@bitbucket.org>
2014-03-11 13:17:31 -0400

    removed expired notifications from api call

--- notifications/helpers.py
+++ notifications/helpers.py
@@ -1,3 +1,5 @@
+import datetime
+from django.db.models import Q
 from api.helpers import DATETIME_FORMAT
 from notifications.models import Notification
 
@@ -7,6 +9,11 @@ def getNotificationsJson(user, since=None):
     notifications = Notification.objects.filter(users=user)
     if since is not None:
         notifications = notifications.filter(date__gt=since)
+    else:
+        threeDaysAgo = datetime.datetime.now() - datetime.timedelta(days=3)
+        notifications = notifications.filter(date__gt=threeDaysAgo)
+        now = datetime.datetime.now()
+        notifications = notifications.filter(Q(status__isnull=True) | Q(status__expires__gt=now))
 
     notifs = list()
     for notif in notifications:
