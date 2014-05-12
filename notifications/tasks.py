3cacb8f946b777f5698c2d59a7aebb77e960a267
George Muresan <gmuresan3@gmail.com>
2014-05-12 15:16:58 -0400

    removed debug print commands

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -8,8 +8,6 @@ from notifications.models import PushNotifications, GCMDevice, APNSDevice
 def handlePushNotification(pushNotificationId):
     try:
         pushNotification = PushNotifications.objects.get(pk=pushNotificationId)
-        print(pushNotificationId)
-        print(pushNotification)
 
         if pushNotification.pushNotificationType == PushNotifications.PUSH_NOTIF_CHAT:
             sendChatNotification(pushNotification)
@@ -19,36 +17,23 @@ def handlePushNotification(pushNotificationId):
 
 def sendChatNotification(pushNotification):
     try:
-        print("handling chat")
-
         message = pushNotification.chatMessage
         conversation = message.conversation
         userProfile = message.user
 
-        print(userProfile)
-
         audience = conversation.members.all()
         audience = audience.exclude(pk=userProfile.pk)
-
-        print(audience)
 
         pushNotification.receivingUsers.add(*audience)
 
         androidDevices = GCMDevice.objects.filter(user__in=audience)
         iosDevices = APNSDevice.objects.filter(user__in=audience)
 
-        print(iosDevices)
-
         messageContents = str(pushNotification)
         extra = {'id': conversation.id, 'type': 'chat', 'userid': message.user.id}
 
-        print(messageContents)
-        print(extra)
-
         androidResponse = androidDevices.send_message(messageContents, extra=extra)
         iosResponse = iosDevices.send_message(messageContents, extra=extra)
-
-        print(iosResponse)
 
         return androidResponse, iosResponse
 
