14bd3a000d03b92c7c240bde72e4be48a5d64f1b
Ahsan <ahsansohail22@gmail.com>
2014-05-12 18:10:52 -0400

    added terms of service, privpol, links to facebook and twitter pages

--- buddyup/urls.py
+++ buddyup/urls.py
@@ -3,15 +3,20 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from buddyup.views import about, tos, privacyPolicy, index
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'buddyup.views.index', name='home'),
+    url(r'^home/$', index,{},'index'),
+    url(r'^about/$', about, {}, 'about'),
+    url(r'^tos/$', tos, {}, 'tos'),
+    url(r'^privacypolicy/$', privacyPolicy, {}, 'privacypolicy'),
     url(r'^api/', include('chat.urls')),
     url(r'^api/', include('userprofile.urls')),
     url(r'^api/', include('status.urls')),
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- buddyup/views.py
+++ buddyup/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'index.html')
+
+def about(request):
+
+    return render(request, 'about.html')
+
+def tos(request):
+
+    return render(request, 'tos.html')
+
+def privacyPolicy(request):
+
+    return render(request, 'privacy_policy.html')
+
