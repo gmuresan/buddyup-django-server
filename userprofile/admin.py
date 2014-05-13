e48294b8b02697e8c7f8740e6b5f4b4dfd0ab734
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 14:35:00 -0400

    chuserprof remove many to many

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -3,14 +3,14 @@ from userprofile.models import UserProfile, Group, Feedback, Setting
 
 class UserProileAdmin(admin.ModelAdmin):
     list_display = ('user','id',)
-    readonly_fields = ('id',)
+    readonly_fields = ('id', 'user', 'friends', 'blockedFriends')
 
 admin.site.register(UserProfile, UserProileAdmin)
 admin.site.register(Group)
 
 class FeedBackAdmin(admin.ModelAdmin):
     list_display = ('text', 'user', 'date',)
-    readonly_fields = ('date',)
+    readonly_fields = ('user', 'date', )
 
 admin.site.register(Feedback, FeedBackAdmin)
 admin.site.register(Setting)
