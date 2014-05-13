3cfb06276dc10aa5af4f2ff1aa6ba10fcbfc42fe
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 10:21:16 -0400

    test links

--- status/admin.py
+++ status/admin.py
@@ -3,6 +3,7 @@ from status.models import Status, Location, Poke
 
 class StatusAdmin(admin.ModelAdmin):
     list_display = ('text', 'user')
+    list_display_links = ('user')
 
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Location)
