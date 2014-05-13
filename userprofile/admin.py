2ebefa94431c9418a3364168800086cfc04071a5
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 10:16:46 -0400

    test

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from userprofile.models import UserProfile, Group, Feedback, Setting
 
 class UserProileAdmin(admin.ModelAdmin):
+    list_display = ('user','id',)
     readonly_fields = ('id',)
 
 admin.site.register(UserProfile, UserProileAdmin)
