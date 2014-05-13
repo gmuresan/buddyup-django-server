e8d932c447f699c72ded315794e6e3aba43ce03a
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 11:14:30 -0400

    test

--- status/admin.py
+++ status/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from status.models import Status, Location, Poke
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('text', 'user', 'starts', 'expires', 'location',)
+    list_display = ('text', 'user', 'starts', 'expires', 'location', 'attendingCount')
+
+    def attendingCount(self, obj):
+        return obj.attending.count()
 
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Location)
