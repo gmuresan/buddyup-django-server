f27cbe7e536a68192d8d8ed061b1114d42f44a13
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 11:29:42 -0400

    testing invited num

--- status/admin.py
+++ status/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from status.models import Status, Location, Poke
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('text', 'user', 'starts', 'expires', 'location', 'attendingCount')
+    list_display = ('text', 'user', 'starts', 'expires', 'location', 'attendingCount', 'invitedcount')
 
     def attendingCount(self, obj):
         return obj.attending.count()
+
+    def invitedCount(self, obj):
+        return obj.invited.count()
 
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Location)
