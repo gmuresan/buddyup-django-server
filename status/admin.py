67a6bc4b2e78ac1186a50103278bfcfd2f708ac6
Richard Zhang <richardtzhang@gmail.com>
2014-05-13 10:41:00 -0400

    fix bug

--- status/admin.py
+++ status/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from status.models import Status, Location, Poke
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('text', 'user', 'starts', 'expires', 'location', 'attending',)
+    list_display = ('text', 'user', 'starts', 'expires', 'location',)
 
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Location)
